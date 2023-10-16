4e093cd15d5112df8408e646d0a7bcad989216bd
Jason <git@thebowstones.net>
2023-10-16 12:36:10 +0100

    Fix bingo because someone broke it

--- bot/cogs/bingo_card.py
+++ bot/cogs/bingo_card.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from io import BytesIO
-from PIL import Image, ImageDraw, ImageFont
+
 from typing import List
 from random import shuffle
 
@@ -13,58 +13,52 @@ class Bingo(commands.Cog):
     def __init__(self, client: Bot):
         self.client = client
 
-    def create(self, values=List[str], x=5, y=5) -> BytesIO:
-        """Draw a XxY bingo sheet with the given values using pillow and return it as a bytesIO object"""
+    def draw_square(text, font, size):
+        '''
+        Draw a single bingo square
+        '''
+        square = Image.new("RGB", (size, size), color=(66,69,73))
+        draw = ImageDraw.Draw(square)
 
-        values: List[str] = values
-        # Only take a random sample of x*y-1 values
-        shuffle(values)
-        values = values[:x * y - 1]
+        draw.line([(0,0), (0, size)], fill=(255,255,255), width=2)
+        draw.line([(0,size), (size, size)], fill=(255,255,255), width=2)
+        draw.line([(size,size), (size, 0)], fill=(255,255,255), width=2)
+        draw.line([(size,0), (0, 0)], fill=(255,255,255), width=2)
 
-        # Create a new image
-        image = Image.new("RGB", (x * 100, y * 100), color="white")
-        draw = ImageDraw.Draw(image)
+        # Fill is python's built in text wrapping feature. Isn't that amazing? Wow!
+        # TODO?: Better alignment
+        draw.text((4,4), fill(text, width=10), font=font, fill=(255,255,255))
+        return square
 
-        # Draw the grid
-        for i in range(0, 500, 100):
-            draw.line((0, i, 500, i), fill="black", width=5)
-            draw.line((i, 0, i, 500), fill="black", width=5)
+    def draw_card(items_per_row: int, card_pixel_height: int, bingo_items: list[str]):
+        '''
+        Draw a bingo card by selecting random items from a list
+        '''
+        number_of_items: int = items_per_row**2
+        square_size: int = floor(card_pixel_height / items_per_row)
 
-        # Load the font
-        font = ImageFont.truetype("arial.tff" if self.client.is_dev else "/usr/share/fonts/truetype/msttcorefonts/Arial.ttf", 15)
+        chosen_items: list[str] = sample(bingo_items, number_of_items)
+        font = ImageFont.truetype("OpenSans-Medium.ttf", floor(card_pixel_height/20))
+        card_image = Image.new("RGB", (card_pixel_height, card_pixel_height), color=(66,69,73))
 
-        # Add an "Free Spot" string to the middle of the list
-        values.insert(len(values) // 2, "Free spot")
-        # For some reason this is just a blank space and idk why but it works so I'm not gonna question it TODO: Figure out why this workn't
+        for i in range(number_of_items):
+            if i == floor(number_of_items/2):
+                square_image = draw_square("Free Space", font, square_size) # The free space
+            else:
+                square_image = draw_square(chosen_items[i], font, square_size)
 
-        # Add text to the image
-        for i, value in enumerate(values):
-            x = (i % x) * 100 + 50
-            y = (i // y) * 100 + 50
-            # if text is longer than box width, split it into multiple lines (as a loop) but does not split words
-            if len(value) > 10:
-                words = value.split(" ")
-                lines = []
-                line = ""
-                for word in words:
-                    if len(line + word) > 10:
-                        lines.append(line)
-                        line = ""
-                    line += word + " "
-                lines.append(line)
-                for j, line in enumerate(lines):
-                    draw.text((x, y + (j - len(lines) // 2) * 15), line, fill="black", font=font, anchor="mm")
+            card_image.paste(square_image, ((i % items_per_row)*square_size, (i // items_per_row)*square_size))
 
         # Save image to io buffer
         buffer = BytesIO()
-        image.save(buffer, format="PNG")
+        card_image.save(buffer, format="PNG")
         buffer.seek(0)
         return buffer
 
     @commands.command()
     async def bingo(self, ctx: commands.Context):
         """Create a random bingo card"""
-        buffer = self.create(BINGO, 3, 3)
+        buffer = self.draw_card(3, 300, BINGO)
         await ctx.send(file=discord.File(buffer, filename="bingo.png"))
 
 Cog = Bingo
